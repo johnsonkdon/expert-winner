5e38cd4dfe065c1b78633f027d4fa10539c1a54e
DONALD <JOHNSON.K.DONALD@GMAIL.COM>
2016-08-11 19:24:35 -0700

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name expertWinnerApp
+ * @description
+ * # expertWinnerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('expertWinnerApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name expertWinnerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the expertWinnerApp
+ */
+angular.module('expertWinnerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
