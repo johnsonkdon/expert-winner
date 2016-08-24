53515d3e89ecdd81587d9ff5e3ef9d746bb0c58f
DONALD <JOHNSON.K.DONALD@GMAIL.COM>
2016-08-24 15:36:03 -0700

    updates

--- app/scripts/controllers/current.js
+++ app/scripts/controllers/current.js
@@ -15,28 +15,34 @@ angular.module('expertWinnerApp')
         cityID: $routeParams.cityID
     });
     $scope.saveCity = function(city){
-    var cityData = {
-        'name': city.name,
-        'id': city.id
-    };
-    if (!$localStorage.savedCities){
-        $localStorage.savedCities = [cityData];
-    } else {
-        // We have already saved some cities.
-        // Check to make sure we haven't already saved the current city.
-        var save = true; // Initialize the save decision variable.
-        // Use this loop to check if we've already saved the city.
-        for (var i=0; i < $localStorage.savedCities.length; i++){
-            if ($localStorage.savedCities[i].id == cityData.id) {
-                // This is a duplicate, so don't save (variable set to false).
-                save = false;
+        var cityData = {
+            'name': city.name,
+            'id': city.id
+        };
+        if (!$localStorage.savedCities){
+            $localStorage.savedCities = [cityData];
+        } else {
+            // Check to make sure we haven't already saved the city.
+            var save = true;
+            for (var i=0; i < $localStorage.savedCities.length; i++){
+                if ($localStorage.savedCities[i].id === cityData.id) {
+                    // this is a duplicate, so don't save
+                    save = false;
+                }
+            }
+            if (save===true){
+                $localStorage.savedCities.push(cityData);
+                // Add object to trigger messages
+                $scope.citySaved = {
+                    'success': true
+                };
+            } else {
+                console.log('city already saved');
+                // Add object to trigger messages
+                $scope.citySaved = {
+                    'duplicate': true
+                };
             }
         }
-        if (save==true){
-            $localStorage.savedCities.push(cityData);
-        } else {
-            console.log('city already saved');
-        }
     }
-};
-  });
+});
